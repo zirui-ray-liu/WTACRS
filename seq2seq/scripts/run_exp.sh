6b980ebcafddef4255374d0f3494fb09bf05d9b3
Guanchu Wang <hegsns@gmail.com>
2023-05-16 21:39:59 -0500

    update repo

--- seq2seq/scripts/run_exp.sh
+++ seq2seq/scripts/run_exp.sh
@@ -6,18 +6,15 @@ model="t5-base"
 # model="bert-large-cased"
 seed_buf=(0 1 2)
 dataset_buf=("rte" "mrpc" "stsb" "cola" "sst2" "qnli" "qqp" "mnli")
-
+ 
 for seed in ${seed_buf[@]}
 do
     for dataset in ${dataset_buf[@]}
     do
-        for batchsize in ${batchsize_buf[@]}
-        do
-            # bash scripts/approx_linear_bert.sh 0 $dataset $model $level $sample_ratio $seed
-            # bash scripts/lora_approx_linear_bert.sh 0 $dataset $model $level $sample_ratio $seed
-            bash scripts/approx_linear.sh 0 $dataset $model $level $sample_ratio $seed
-            # bash scripts/lora_approx_linear.sh 0 $dataset $model $level $sample_ratio $seed
-        done
+        # bash scripts/approx_linear_bert.sh 0 $dataset $model $level $sample_ratio $seed
+        # bash scripts/lora_approx_linear_bert.sh 0 $dataset $model $level $sample_ratio $seed
+        bash scripts/approx_linear.sh 0 $dataset $model $level $sample_ratio $seed
+        # bash scripts/lora_approx_linear.sh 0 $dataset $model $level $sample_ratio $seed
     done
 done
 
