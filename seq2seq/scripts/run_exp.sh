9e3e839bc07994f8cf547c549349033ffcaf6b5a
Guanchu Wang <hegsns@gmail.com>
2023-05-16 21:34:20 -0500

    update repo

--- seq2seq/scripts/run_exp.sh
+++ seq2seq/scripts/run_exp.sh
@@ -1,12 +1,11 @@
 level=2
-sample_ratio=0.1
-# model="roberta-base"
-# model="t5-small"
+sample_ratio=0.3
+model="t5-base"
 # model="bert-base-cased"
-model="t5-3b"
-seed_buf=(9)
-dataset_buf=("rte") # "rte" "mrpc" "stsb" "cola") # "sst2" "qnli" "qqp" "mnli") # ("rte") # "mrpc" "stsb" "cola" "sst2") #
-batchsize_buf=(38 32 16 8)
+# model="t5-large"
+# model="bert-large-cased"
+seed_buf=(0 1 2)
+dataset_buf=(rte" "mrpc" "stsb" "cola" "sst2" "qnli" "qqp" "mnli")
 
 for seed in ${seed_buf[@]}
 do
@@ -14,21 +13,14 @@ do
     do
         for batchsize in ${batchsize_buf[@]}
         do
-            # bash scripts/approx_linear_bert.sh 3 $dataset $model $level $sample_ratio $seed
+            # bash scripts/approx_linear_bert.sh 0 $dataset $model $level $sample_ratio $seed
             # bash scripts/lora_approx_linear_bert.sh 0 $dataset $model $level $sample_ratio $seed
-            # bash scripts/approx_linear.sh 1 rte $model $level $sample_ratio $seed
-            bash scripts/lora_approx_linear_3b.sh 0 $dataset $model $level $sample_ratio $seed $batchsize
+            bash scripts/approx_linear.sh 0 $dataset $model $level $sample_ratio $seed
+            # bash scripts/lora_approx_linear.sh 0 $dataset $model $level $sample_ratio $seed
         done
     done
 done
 
-# for dataset in ${dataset_buf[@]}
-# do
-#     for seed in ${seed_buf[@]}
-#     do
-#     bash scripts/approx_linear_ablation.sh 0 $dataset $model $level $sample_ratio $seed randomize
-#     done
-# done
 
 
 
---
+++ seq2seq/scripts/run_exp_baseline.sh
@@ -0,0 +1,17 @@
+model="t5-base"
+# model="t5-large"
+# model="bert-base-cased"
+# model="bert-large-cased"
+dataset_buf=(rte" "mrpc" "stsb" "cola" "sst2" "qnli" "qqp" "mnli")
+seed_buf=(0 1 2)
+
+for seed in ${seed_buf[@]}
+do
+    for dataset in ${dataset_buf[@]}
+    do
+        # bash scripts/baseline_bert.sh 0 $dataset $model $seed
+        # bash scripts/lora_baseline_bert.sh 0 $dataset $model $seed
+        bash scripts/baseline.sh 0 $dataset $model $seed
+        # bash scripts/lora_baseline.sh 0 $dataset $model $seed
+    done
+done
